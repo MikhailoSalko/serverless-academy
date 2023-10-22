85feaad245f3b4b60f2132f5956ad6186dadeea1
Mikhailo Salko <slipknotmisha@gmail.com>
2023-10-22 19:29:59 +0300

    first part of task - questions

--- 02_cli_primitive_db/index.js
+++ 02_cli_primitive_db/index.js
@@ -1,8 +1,27 @@
 import inquirer from "inquirer";
 import fs from "fs/promises";
 
+const question = [
+  {
+    type: "input",
+    name: "name",
+    message: "Please, enter your name",
+  },
+  {
+    type: "list",
+    name: "gender",
+    message: "Please, choose your gender",
+    choices: ["male", "female"],
+  },
+  {
+    type: "input",
+    name: "age",
+    message: "Please, enter your age",
+  },
+];
+
 const enterName = async () => {
-  const msg = await inquirer.prompt([{ name: "Please, enter the name of the user" }]);
+  const msg = await inquirer.prompt(question);
   console.log(msg);
 };
 
