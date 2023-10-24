658781a139f487b669577cb786e9318cd62635e6
Mikhailo Salko <slipknotmisha@gmail.com>
2023-10-24 15:05:42 +0300

    little changes to find the solution

--- 03_cli_telegramm_bot/index.js
+++ 03_cli_telegramm_bot/index.js
@@ -1,24 +1,29 @@
 import { program } from "commander";
 
 const invokeAction = async ({ action, message, photo, help }) => {
-  switch (action) {
-    case "message":
-      return console.log("this is message");
-    case "m":
-      return console.log("this is message");
-    case "photo":
-      return console.log("this is photo");
-    case "p":
-      return console.log("this is photo");
-    case "--help":
-      return console.log("these are all commands");
-    default:
-      console.warn("\x1B[31m Unknown action type!");
+  // console.log(process.argv);
+  if (message) {
+    console.log("this is message: ", message);
   }
+
+  // switch (action) {
+  //   case "message":
+  //     return console.log("this is message: ", message);
+  //   case "m":
+  //     return console.log("this is message");
+  //   case "photo":
+  //     return console.log("this is photo");
+  //   case "p":
+  //     return console.log("this is photo");
+  //   case "--help":
+  //     return console.log("these are all commands");
+  //   default:
+  //     console.warn("\x1B[31m Unknown action type!");
+  // }
 };
 
 program
-  .option("-a, --action <type>", "choose action")
+  // .option("-a, --action <type>", "choose action")
   .option("-m, --message <type>", "user message")
   .option("-p, --photo <type>", "path to photo")
   .option("-h, --help");
@@ -26,5 +31,6 @@ program
 program.parse();
 
 const consoleActions = program.opts();
+console.log(consoleActions);
 
 invokeAction(consoleActions);
