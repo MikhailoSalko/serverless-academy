3937cf324731192ff3a50f80990943b8f95c34a7
Mikhailo Salko <slipknotmisha@gmail.com>
2023-10-24 16:04:27 +0300

    remove nodemon, update script start

--- 03_cli_telegramm_bot/index.js
+++ 03_cli_telegramm_bot/index.js
@@ -1,4 +1,24 @@
 import { program } from "commander";
+import TelegramBot from "node-telegram-bot-api";
+
+const { TELEGRAM_TOKEN } = process.env;
+
+// const token = "6817217923:AAH_ASwzmN4RLoz31kXWERpXK9hDwGgJUYc";
+// console.log(process.env);
+
+const bot = new TelegramBot(TELEGRAM_TOKEN, { polling: true });
+
+// bot.onText(/\/echo (.+)/, (msg, match) => {
+//   // 'msg' is the received Message from Telegram
+//   // 'match' is the result of executing the regexp above on the text content
+//   // of the message
+
+//   const chatId = msg.chat.id;
+//   const resp = match[1]; // the captured "whatever"
+
+//   // send back the matched "whatever" to the chat
+//   bot.sendMessage(chatId, resp);
+// });
 
 const invokeAction = async ({ action, message, photo, help }) => {
   // console.log(process.argv);
@@ -31,6 +51,6 @@ program
 program.parse();
 
 const consoleActions = program.opts();
-console.log(consoleActions);
+// console.log(consoleActions);
 
-invokeAction(consoleActions);
+// invokeAction(consoleActions);
