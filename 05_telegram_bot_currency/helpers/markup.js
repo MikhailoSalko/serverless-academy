49783e55d950d37d121f49be5ca092cacc3cc980
Mikhailo Salko <slipknotmisha@gmail.com>
2023-10-28 12:43:52 +0300

    add markup func

---
+++ 05_telegram_bot_currency/helpers/markup.js
@@ -0,0 +1,21 @@
+const markup = (list) => {
+  const markup = list
+    .map((el) => {
+      const obj = {
+        UAH: el.base_ccy || "UAH",
+        sale: el.sale || el.rateSell,
+        buy: el.buy || el.rateBuy,
+      };
+      return `
+    -----------------------------------------------------------
+    Base currecy:  ${obj.UAH}         Sale                Buy
+    PrivatBank:               ${obj.sale}       ${obj.sale}
+    Monomank:              ${obj.buy}        ${obj.buy}
+    -----------------------------------------------------------
+    `;
+    })
+    .join("");
+  return markup;
+};
+
+export default markup;
