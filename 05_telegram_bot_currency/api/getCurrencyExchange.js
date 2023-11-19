2fab9e3c42f6ef2fe477df0bf2371e0eaf431010
Mikhailo Salko <slipknotmisha@gmail.com>
2023-11-19 12:00:55 +0200

    delete commented code

--- 05_telegram_bot_currency/api/getCurrencyExchange.js
+++ 05_telegram_bot_currency/api/getCurrencyExchange.js
@@ -15,16 +15,10 @@ const getCurrencyExchange = async () => {
   const { data: mono } = await monobankInstace.get();
   const monoSlice = mono.slice(0, 2);
 
-  // const currency = {
-  //   dollar: [privat[0], monoSlice[1]],
-  //   euro: [privat[1], monoSlice[0]],
-  // };
-
   return {
     dollar: [privat[1], monoSlice[0]],
     euro: [privat[0], monoSlice[1]],
   };
-  // return [...privat, ...mono.slice(0, 2)];
 };
 
 export default getCurrencyExchange;
